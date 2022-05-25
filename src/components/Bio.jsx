370a4d728e6384b72271a98ede9d1af0910e0505
Keelan <keelanmatthews123@gmail.com>
2022-05-25 12:13:45 +0200

    Character scroll functionality

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,25 +6,27 @@ import StatInfo from './StatInfo';
 import AbilityInfo from './AbilityInfo';
 
 export default function Bio() {
-  $(window).scroll(function(e){ 
-    var $el = $('.background'); 
-    var isPositionFixed = ($el.css('position') === 'fixed');
-    if ($(this).scrollTop() > 1000 && !isPositionFixed){ 
-      $el.css({'position': 'fixed', 'transform': 'translateY(1080px)'}); 
-    }
-    if (($(this).scrollTop() < 1080 || $(this).scrollTop() > 3240) && isPositionFixed){
-      $el.css({'position': 'static', 'transform': 'translateY(0px)'}); 
-    } 
-  });
+	$(window).scroll(function (e) {
+		var $el = $('.background');
+		var isPositionFixed = ($el.css('position') === 'fixed');
+		if ($(this).scrollTop() > 1000 && !isPositionFixed) {
+			$el.css({ 'position': 'fixed', 'transform': 'translateY(1080px)' });
+		}
+		if (($(this).scrollTop() < 1080 || $(this).scrollTop() > 3240) && isPositionFixed) {
+			$el.css({ 'position': 'static', 'transform': 'translateY(0px)' });
+		}
+	});
 
-  return (
-    <section className="">
-      <div className="vh-100">
-          <img src={Background} className="background" alt="" />
-      </div>
-      <BioInfo/>
-      <StatInfo/>
-      <AbilityInfo/>
-    </section>
-  )
+	return (
+		<section className="">
+			<div className="vh-100">
+				<img src={Background} className="background" alt="" />
+			</div>
+			<div className="scroll-container">
+				<BioInfo className="scroll-area" />
+				<StatInfo className="scroll-area" />
+				<AbilityInfo className="scroll-area" />
+			</div>
+		</section>
+	)
 }
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -3,33 +3,47 @@ import CharacterCard from './CharacterCard';
 import Gorilla from '../images/gorilla.svg';
 import Fly from '../images/fly.svg';
 import Giraffe from '../images/giraffe.svg';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 export default function Characters() {
-  return (
-    <section className="d-flex vh-100 align-items-center" style={{backgroundColor: '#354856'}}>
-      <div className="d-flex flex-column w-100 container">
-            <h1 className="display-1 fw-bold text-center text-white mb-5 pb-5">Characters</h1>
-            <div className="d-flex w-100">
-                <CharacterCard 
-                    character="Gojo Gorira" 
-                    image={Gorilla} 
-                    business="Anima Bank"
-                    occupation="Executive Manager"
-                    fact="Does not like to do any work himself"/>
-                <CharacterCard 
-                    character="Tobias Tobu" 
-                    image={Fly} 
-                    business="Local Bar"
-                    occupation="Bartender/Crypto Enthusiast"
-                    fact="Is happy to make customers do the dishes"/>
-                <CharacterCard 
-                    character="Katarina Kirin" 
-                    image={Giraffe} 
-                    business="Anima Bank"
-                    occupation="Chief Executive Officer"
-                    fact="her long neck leaves her succeptible to danger"/>
-            </div>
-      </div>
-    </section>
-  )
+
+	const child = { width: `78em`, height: `100%` }
+	const parent = { width: `1500px`, height: `68%` }
+
+	return (
+		<section className="d-flex flex-column vh-100 align-items-center" style={{ backgroundColor: '#354856' }}>
+				<h1 className="display-1 fw-bold text-center text-white mb-5 pb-5">Characters</h1>
+				<div style={parent} className="ms-5 ps-5">
+					<HorizontalScroll reverseScroll={true}>
+						<div style={child}>
+							<CharacterCard
+								character="Gojo Gorira"
+								image={Gorilla}
+								business="Anima Bank"
+								occupation="Executive Manager"
+								fact="Does not like to do any work himself" />
+						</div>
+						<div style={child}>
+							<CharacterCard
+								character="Tobias Tobu"
+								image={Fly}
+								business="Local Bar"
+								occupation="Bartender/Crypto Enthusiast"
+								fact="Is happy to make customers do the dishes" />
+						</div>
+						<div style={child}>
+							<CharacterCard
+								character="Katarina Kirin"
+								image={Giraffe}
+								business="Anima Bank"
+								occupation="Chief Executive Officer"
+								fact="her long neck leaves her succeptible to danger" />
+						</div>
+					</HorizontalScroll>
+				</div>
+				<div className="posiiton-relative">
+					<div className='icon-scroll'></div>
+				</div>
+		</section>
+	)
 }
